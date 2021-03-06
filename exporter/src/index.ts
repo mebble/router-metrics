23b7289a6b849c09d6d38f571cedd1f3f34dd7f3
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-03-06 23:33:56 +0530

    load vars from env

--- exporter/src/index.ts
+++ exporter/src/index.ts
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
+dotenv.config();
+
 const app = express();
-const port = 5000;
+const port = parseInt(process.env.NODE_SERVER_PORT, 10);
 
 app.get('/ping', (req, res) => {
-    res.send('ponge!');
+    res.send('pong!');
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
