dfbbc6ea1d34905a968b5059d047c0e912c55c68
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-03-07 01:53:19 +0530

    move base input out

--- exporter/src/mapper.spec.ts
+++ exporter/src/mapper.spec.ts
@@ -1,20 +1,20 @@
 import { DeviceOnline, DeviceMetricLabels } from './types';
 import { downSpeedMetric } from './mapper';
 
-describe('downSpeedMetric', () => {
-    const baseInput: DeviceOnline = {
-        qosListHostname: "hostname",
-        qosListRemark: "",
-        qosListIP: "ip",
-        qosListConnectType: "conn-type",
-        qosListMac: "mac",
-        qosListDownSpeed: "1.2",
-        qosListUpSpeed: "1.3",
-        qosListDownLimit: "100.00",
-        qosListUpLimit: "200.00",
-        qosListAccess: "list-access",
-    };
+const baseInput: DeviceOnline = {
+    qosListHostname: "hostname",
+    qosListRemark: "",
+    qosListIP: "ip",
+    qosListConnectType: "conn-type",
+    qosListMac: "mac",
+    qosListDownSpeed: "1.2",
+    qosListUpSpeed: "1.3",
+    qosListDownLimit: "100.00",
+    qosListUpLimit: "200.00",
+    qosListAccess: "list-access",
+};
 
+describe('downSpeedMetric', () => {
     test('should map metric labels', () => {
         const expectedLabels: DeviceMetricLabels = {
             deviceName: baseInput.qosListHostname,
