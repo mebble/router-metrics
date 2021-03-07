80ba06125de0e4fd5c7e0427172569150fe50b08
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-03-07 13:07:57 +0530

    extract out fetcher stubbing

--- exporter/src/router.spec.ts
+++ exporter/src/router.spec.ts
@@ -1,20 +1,24 @@
 import sinon from 'sinon';
 import { Response } from 'node-fetch';
-import { DeviceOnline } from './types';
+import { Fetcher, DeviceOnline } from './types';
 import { router } from './router';
+
+const stubFetcher = (devices: DeviceOnline[]): Fetcher => {
+    const upstreamBody = JSON.stringify({ onlineList: devices });
+    const response = new Response();
+    sinon.stub(response, "text").returns(Promise.resolve(upstreamBody));
+    const fetch = sinon.stub().returns(Promise.resolve(response));
+
+    return fetch;
+};
 
 afterEach(() => {
     sinon.restore();
 });
 
 describe('getDevices', () => {
-    test('should return empty array when upstream response has empty onlineList', async () => {
-        const upstreamResponse = JSON.stringify({
-            onlineList: new Array<DeviceOnline>()
-        });
-        const response = new Response();
-        sinon.stub(response, "text").returns(Promise.resolve(upstreamResponse));
-        const fetch = sinon.stub().returns(Promise.resolve(response));
+    test('should return empty array when upstream response returns empty onlineList', async () => {
+        const fetch = stubFetcher([]);
         const getDevices = router(fetch);
 
         const res = await getDevices();
