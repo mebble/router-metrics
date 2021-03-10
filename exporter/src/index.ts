7a830000c4640dd3046349f0f3c89fe32dfadd65
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-03-10 21:42:20 +0530

    fix - move config load to top of application

--- exporter/src/index.ts
+++ exporter/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
-import { createApp } from './app';
-
 dotenv.config();
+
+import { createApp } from './app';
 
 const port = parseInt(process.env.NODE_SERVER_PORT, 10);
 const app = createApp()
