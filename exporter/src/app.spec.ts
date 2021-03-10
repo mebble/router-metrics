b661b2970017e0295cb641ba1e5bc20b756c0425
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-03-10 20:30:03 +0530

    update app spec to refer to download metric

--- exporter/src/app.spec.ts
+++ exporter/src/app.spec.ts
@@ -18,12 +18,12 @@ describe('app ping', () => {
     });
 });
 
-describe('app metrics', () => {
+describe('app download metrics', () => {
     const routerMock = nock(`http://${process.env.ROUTER_HOST}:${process.env.ROUTER_PORT}`);
 
     test('should return no metrics when the router returns empty online devices list', async () => {
         routerMock.get(RouterPaths.OnlineList)
-            .reply(200, '{"onlineList": []}', { 'content-type': 'text/plain' })
+            .reply(200, '{"onlineList": []}', { 'content-type': 'text/plain' });
         const expectedResponse = dedent`
             # HELP ${DownloadMetric.name} ${DownloadMetric.help}
             # TYPE ${DownloadMetric.name} gauge
